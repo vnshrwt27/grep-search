8762f3c4498a862cc2e04adce62bd2b201c8f090
Vansh Rawat <vanshrawat643@gmail.com>
2026-01-14 17:59:25 +0000

    basic pattern matching in file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{env, io::{BufRead, Write, stdin, stdout}};
 fn main() {
-    println!("Hello, world!");
+    let stdin = stdin();
+    let pattern = env::args() 
+        .nth(1).unwrap();
+    let mut stdout = stdout() ;
+    for line in stdin.lock().lines(){
+        let line = line.expect("Error reading line") ;
+
+        if line.contains(&pattern){
+            write!(stdout,"{}\n",line).unwrap();
+            stdout.flush().unwrap();
+
+        }
+    }
+         
+
 }
